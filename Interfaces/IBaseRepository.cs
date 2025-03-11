7a783671f3b4ad69aeaab3b53952e2efd52c6ee1
Mariam Mohammed <mariemnasser45@gmail.com>
2025-03-11 09:55:54 +0200

    Base Repository Functions

--- Interfaces/IBaseRepository.cs
+++ Interfaces/IBaseRepository.cs
@@ -2,5 +2,12 @@
 {
     public interface IBaseRepository<T> where T : class
     {
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAllWithData();
+        Task<T> FindById(int id);
+        Task<T> FindByIdWithData(int id);
     }
 }
--- Services/BaseRepository..cs
+++ Services/BaseRepository..cs
@@ -1,4 +1,5 @@
-﻿using MVCWebApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWebApp.Data;
 using MVCWebApp.Interfaces;
 
 namespace MVCWebApp.Services
@@ -11,5 +12,98 @@ namespace MVCWebApp.Services
         {
             _context=context;
         }
+
+        public async Task<T> Add(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return entity;
+        }
+
+        public async Task<T> FindById(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+
+            if (entity==null)
+                return null;
+            else
+                _context.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+            return entity;
+        }
+
+        public async Task<T> FindByIdWithData(int id)
+        {
+            var collection = await GetCollection(typeof(T));
+
+            var entry  = await FindById(id);
+
+            if (entry==null)
+                return null;
+            else
+            {
+                IQueryable<T> queryable =  _context.Set<T>().AsNoTracking();
+
+                foreach(var col in collection )
+                {
+                    queryable = queryable.Include(col).AsNoTracking();
+                }
+
+                var entity =await queryable.Where(i => i.Equals(entry)).AsNoTracking().SingleOrDefaultAsync();
+
+                _context.Entry(entity).State = EntityState.Detached;
+
+                return entity;
+            }
+
+        }
+
+        public async Task<List<string>> GetCollection(Type type)
+        {
+            var collection = type.GetProperties()
+                .Where(p=>(typeof(T).IsAssignableFrom(p.PropertyType)
+                && p.PropertyType!=typeof(string)
+                && p.PropertyType != typeof(byte[])
+                && p.PropertyType.Namespace==type.Namespace)).Select(n=>n.Name).ToList();
+
+            return collection;
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            var entities = await _context.Set<T>().ToListAsync();
+
+            if (entities.Count()==0)
+                return null;
+
+            return entities;
+
+        }
+
+        public async Task<IEnumerable<T>> GetAllWithData()
+        {
+            var entries = _context.Set<T>().AsNoTracking().AsQueryable();
+
+            var includes = await GetCollection(typeof(T));
+
+            foreach(var inc in includes)
+            {
+                entries.Include(inc);
+            }
+
+            return await entries.ToListAsync();
+
+        }
+        public async Task<T> Update(T entity)
+        {
+             _context.Set<T>().Update(entity);
+
+            return entity;
+
+        }
+
     }
 }
